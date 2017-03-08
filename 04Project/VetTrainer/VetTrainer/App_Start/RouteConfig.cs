451000a6bbd40bdd5c6d4cf570ecd8b6c37bd653
chesterz184 <chesterz0310@hotmail.com>
2017-03-08 15:31:12 +0800

    initialize

--- 04Project/VetTrainer/VetTrainer/App_Start/RouteConfig.cs
+++ 04Project/VetTrainer/VetTrainer/App_Start/RouteConfig.cs
@@ -19,7 +19,7 @@ namespace VetTrainer
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
--- 04Project/VetTrainer/VetTrainer/Controllers/HomeController.cs
+++ 04Project/VetTrainer/VetTrainer/Controllers/HomeController.cs
@@ -6,13 +6,22 @@ using System.Web.Mvc;
 
 namespace VetTrainer.Controllers
 {
+    [RoutePrefix("Home")]
     public class HomeController : Controller
     {
+        [Route("~/")]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Route("~/Login")]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        
         public ActionResult About()
         {
             ViewBag.Message = "制作组：115happy.";
