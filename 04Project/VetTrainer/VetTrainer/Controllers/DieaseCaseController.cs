d9651861af0e8cb14cc1c8eeec934a11bfd38341
Your Name <you@example.com>
2017-03-15 23:52:55 +0800

    disease_case

---
+++ 04Project/VetTrainer/VetTrainer/Controllers/DieaseCaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VetTrainer.Controllers
+{
+    public class DieaseCaseController : Controller
+    {
+        // GET: DieaseCase
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
