c6b4c577f6e2bd9c10038698c8ab4b4e49c91a7b
clivic <thomasravel@sina.com>
2017-03-15 14:49:09 +0800

    2017/3/15 Added Nav Controller.

---
+++ 04Project/VetTrainer/VetTrainer/Controllers/NavController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VetTrainer.Controllers
+{
+    [RoutePrefix("nav")]
+
+    public class NavController : Controller
+    {
+        // GET: Navigation
+        [Route("chooseclinic")]
+        public ActionResult ChooseClinic()
+        {
+            return View();
+        }
+    }
+}
