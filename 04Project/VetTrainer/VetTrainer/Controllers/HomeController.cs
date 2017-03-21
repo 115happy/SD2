c458a94e8b95d701fc8dca795640916a84b0c220
clivic <thomasravel@sina.com>
2017-03-21 11:57:30 +0800

    2017/3/21 Modified some in User login functionality.

--- 04Project/VetTrainer/VetTrainer/Controllers/HomeController.cs
+++ 04Project/VetTrainer/VetTrainer/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VetTrainer.Utilities;
 
 namespace VetTrainer.Controllers
 {
---
+++ 04Project/VetTrainer/VetTrainer/Utilities/CheckAuthorization.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VetTrainer.Utilities
+{
+    public class CheckAuthorization : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (HttpContext.Current.Session["UserID"] == null || !HttpContext.Current.Request.IsAuthenticated)
+            {
+                if (filterContext.HttpContext.Request.IsAjaxRequest())
+                {
+                    filterContext.HttpContext.Response.StatusCode = 302; //Found Redirection to another page. Here- login page. Check Layout ajaxError() script.
+                    filterContext.HttpContext.Response.End();
+                }
+                else
+                {
+                    //filterContext.Result = new RedirectResult(System.Web.Security.FormsAuthentication.LoginUrl + "?ReturnUrl=" +
+                    //     filterContext.HttpContext.Server.UrlEncode(filterContext.HttpContext.Request.RawUrl));
+                    filterContext.Result = new RedirectResult("index");
+                }
+            }
+            else
+            {
+
+                //Code HERE for page level authorization
+
+            }
+        }
+    }
+}
