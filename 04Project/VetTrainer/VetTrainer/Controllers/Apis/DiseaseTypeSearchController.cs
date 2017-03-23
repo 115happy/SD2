c195ac69430a04bd7ec5ee46f6672e26cb0efb83
Mhhli <252126403@qq.com>
2017-03-23 10:21:58 +0800

    commit

--- 04Project/VetTrainer/VetTrainer/Controllers/Apis/DiseaseTypeSearchController.cs
+++ 04Project/VetTrainer/VetTrainer/Controllers/Apis/DiseaseTypeSearchController.cs
@@ -16,7 +16,7 @@ namespace VetTrainer.Controllers.Apis
     public class DiseaseTypeSearchController : ApiController
     {
         VetAppDBContext _context = new VetAppDBContext();
-        // GET: RoleSearch
+
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
@@ -64,56 +64,53 @@ namespace VetTrainer.Controllers.Apis
             var str = "{ \"Message\" : \"" + msg + "\" , \"" + "Data\" : " + jss.Serialize(DiseaseTypeDtos) + " }";
             return Ok(str);
         }
-        //获取查询用户信息结果api
-        public IHttpActionResult GetSearchResult(string searchText)
-        {
-            string msg = "";
-            var clinicDtos = new List<ClinicDto>();
-            try
-            {
-                List<Clinic> clinics = _context.Clinics.Include(u => u.Instruments)
-                    .Include(u => u.Texts).Include(u => u.Pictures)
-                    .Include(u => u.Videos).ToList();
+        //public IHttpActionResult GetSearchResult(string searchText)
+        //{
+        //    string msg = "";
+        //    var DiseaseTypeDtos = new List<DiseaseTypeDto>();
+        //    try
+        //    {
+        //        List<DiseaseType> diseaseTypes = new List<DiseaseType>();
 
-                if (searchText == null || searchText.Trim() == "")
-                {
-                    clinics = _context.Clinics.Include(u => u.Instruments)
-                    .Include(u => u.Texts).Include(u => u.Pictures)
-                    .Include(u => u.Videos).ToList();
-                }
-                else
-                {
-                    clinics = _context.Clinics.Where(u => u.Name.Contains(searchText)).Include(u => u.Instruments)
-                    .Include(u => u.Texts).Include(u => u.Pictures)
-                    .Include(u => u.Videos).ToList();
-                }
-                foreach (Clinic c in clinics)
-                {
-                    foreach (Instrument i in c.Instruments)
-                    {
-                        _context.Entry(i).Collection(u => u.Texts).Load();
-                        _context.Entry(i).Collection(u => u.Pictures).Load();
-                        _context.Entry(i).Collection(u => u.Videos).Load();
-                    }
-                }
-                foreach (Clinic clinic in clinics)
-                {
-                    var clinicDto = Mapper.Map<Clinic, ClinicDto>(clinic);
-                    clinicDtos.Add(clinicDto);
-                }
-                if (clinicDtos.Count > 0)
-                    msg = "查找成功";
-                else
-                    msg = "没有结果";
+        //        if (searchText == null || searchText.Trim() == "")
+        //        {
+        //            clinics = _context.Clinics.Include(u => u.Instruments)
+        //            .Include(u => u.Texts).Include(u => u.Pictures)
+        //            .Include(u => u.Videos).ToList();
+        //        }
+        //        else
+        //        {
+        //            clinics = _context.Clinics.Where(u => u.Name.Contains(searchText)).Include(u => u.Instruments)
+        //            .Include(u => u.Texts).Include(u => u.Pictures)
+        //            .Include(u => u.Videos).ToList();
+        //        }
+        //        foreach (Clinic c in clinics)
+        //        {
+        //            foreach (Instrument i in c.Instruments)
+        //            {
+        //                _context.Entry(i).Collection(u => u.Texts).Load();
+        //                _context.Entry(i).Collection(u => u.Pictures).Load();
+        //                _context.Entry(i).Collection(u => u.Videos).Load();
+        //            }
+        //        }
+        //        foreach (Clinic clinic in clinics)
+        //        {
+        //            var clinicDto = Mapper.Map<Clinic, ClinicDto>(clinic);
+        //            clinicDtos.Add(clinicDto);
+        //        }
+        //        if (clinicDtos.Count > 0)
+        //            msg = "查找成功";
+        //        else
+        //            msg = "没有结果";
 
-            }
-            catch (RetryLimitExceededException)
-            {
-                msg = "网络故障";
-            }
-            JavaScriptSerializer jss = new JavaScriptSerializer();
-            var str = "{ \"Message\" : \"" + msg + "\" , \"" + "Data\" : " + jss.Serialize(clinicDtos) + " }";
-            return Ok(str);
-        }
+        //    }
+        //    catch (RetryLimitExceededException)
+        //    {
+        //        msg = "网络故障";
+        //    }
+        //    JavaScriptSerializer jss = new JavaScriptSerializer();
+        //    var str = "{ \"Message\" : \"" + msg + "\" , \"" + "Data\" : " + jss.Serialize(clinicDtos) + " }";
+        //    return ok(str);
+        //}
     }
 }
