593b53aa0f9a936bbc341626bd2bda252c4916e4
Your Name <you@example.com>
2017-03-27 00:16:27 +0800

    update user manage

---
+++ 04Project/VetTrainer/VetTrainer/Controllers/UserManagementController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VetTrainer.Models;
+
+namespace VetTrainer.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        VetAppDBContext _context = new VetAppDBContext();
+        // GET: UserManagement
+        public ActionResult Index(string Name)
+        {
+            User userToUpdate = _context.Users.Where(u => u.Name == Name).FirstOrDefault();
+            ViewBag.User = userToUpdate;
+            return View();
+        }
+    }
+}
