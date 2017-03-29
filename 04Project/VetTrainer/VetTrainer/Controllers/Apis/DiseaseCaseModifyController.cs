7f50173e2ea851066c031a18c1827bc477130017
Mhhli <252126403@qq.com>
2017-03-29 18:04:15 +0800

    diseaseCase

--- 04Project/VetTrainer/VetTrainer/Controllers/Apis/DiseaseCaseModifyController.cs
+++ 04Project/VetTrainer/VetTrainer/Controllers/Apis/DiseaseCaseModifyController.cs
@@ -20,7 +20,7 @@ namespace VetTrainer.Controllers.Apis
             _context.Dispose();
         }
 
-        public IHttpActionResult PostDiseaseTypeModify(DiseaseCaseDto diseaseCase)
+        public IHttpActionResult PostDiseaseCaseModify(DiseaseCaseDto diseaseCase)
         {
             string msg = "";
             if (diseaseCase == null)
@@ -28,6 +28,16 @@ namespace VetTrainer.Controllers.Apis
                 msg = "参数错误";
             }
             var diseaseCaseToUpdate = _context.DiseaseCases.Find(diseaseCase.Id);
+            _context.Entry(diseaseCaseToUpdate).Collection(u => u.DiseaseCaseTabs).Load();
+            
+            foreach(DiseaseCaseTab dct in diseaseCaseToUpdate.DiseaseCaseTabs)
+            {
+                _context.Entry(dct).Collection(u => u.Analyses).Load();
+                _context.Entry(dct).Collection(u => u.Drugs).Load();
+                _context.Entry(dct).Collection(u => u.Texts).Load();
+                _context.Entry(dct).Collection(u => u.Pictures).Load();
+                _context.Entry(dct).Collection(u => u.Videos).Load();
+            }
             diseaseCaseToUpdate.Name = diseaseCase.Name;
 
             try
