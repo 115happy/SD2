adefd1e3b4700ffb50b6eca4fded4fe09a74f191
Your Name <you@example.com>
2017-03-29 13:53:50 +0800

    update case select

--- 04Project/VetTrainer/VetTrainer/Controllers/CaseController.cs
+++ 04Project/VetTrainer/VetTrainer/Controllers/CaseController.cs
@@ -38,7 +38,7 @@ namespace VetTrainer.Controllers
         [Route("CaseLearning")]
         public ActionResult CaseLearning(String diseaseCaseName)
         {
-            var diseaseCases = _context.DiseaseCases.Include(dc => dc.DiseaseCaseTabs).Where(dc => dc.Name == diseaseCaseName);
+            var diseaseCases = _context.DiseaseCases.Include(dc => dc.DiseaseCaseTabs).Where(dc => dc.Name == diseaseCaseName).ToList();
             return View(diseaseCases);
         }
     }
